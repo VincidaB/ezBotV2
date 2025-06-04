a3cdc25f0b6c40740f20302b5b73f229d6540550
Vincent Belpois <vbelpois@traak.tech>
2025-06-04 21:48:39 +0200

    Fixed some docker related issues

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR/..
 
-docker build -f docker/dockerfile-pre-rosdep . -t ezbot-dev-pre-rosdep:latest --progress=plain
+docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -f docker/dockerfile-pre-rosdep . -t ezbot-dev-pre-rosdep:latest --progress=plain
 docker build -f docker/dockerfile-post-rosdep --no-cache . -t ezbot-v2-dev --progress=plain
 #docker build -f docker/dockerfile-post-rosdep --no-cache . -t ezbot-v2-dev --progress=plain
